07ea459e162469c8ba915c663fd1816c9fdfbec1
Karthik <karthik7355116l@gmail.com>
2026-05-24 15:21:01 +0530

    Connected backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : https://smartlead-crm-3p9i.onrender.com
 
   const config = {
     plugins: [react()],
