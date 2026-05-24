7745c73734559ee77075ca8f27af07eaf8deec9f
Karthik <karthik7355116l@gmail.com>
2026-05-24 16:27:58 +0530

    Fixed final env issue

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,23 +1,16 @@
 export const API_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : https://smartlead-crm-3p9i.onrender.com;
-export const BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER
-    : https://smartlead-crm-3p9i.onrender.com
+  'https://smartlead-crm-3p9i.onrender.com/api/';
 
-export const WEBSITE_URL = import.meta.env.PROD
-  ? 'http://cloud.idurarapp.com/'
-  : 'http://localhost:3000/';
+export const BASE_URL =
+  'https://smartlead-crm-3p9i.onrender.com/';
+
+export const WEBSITE_URL =
+  'https://smartlead-crm-plum.vercel.app/';
+
 export const DOWNLOAD_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : https://smartlead-crm-3p9i.onrender.com
+  'https://smartlead-crm-3p9i.onrender.com/download/';
+
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
-export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
-
-//  console.log(
-//    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
-//  );
+export const FILE_BASE_URL =
+  'https://smartlead-crm-3p9i.onrender.com/';
